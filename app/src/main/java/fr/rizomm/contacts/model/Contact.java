9db49ce7253f76232e0c05d75133957984b9d7e4
maximilien marie <maximilien.marie@isen-lille.fr>
2015-03-01 11:59:16 +0100

    refactor: remove parcelable from contact model

--- app/src/main/java/fr/rizomm/contacts/model/Contact.java
+++ app/src/main/java/fr/rizomm/contacts/model/Contact.java
@@ -13,39 +13,9 @@ import lombok.experimental.Builder;
 @Data
 @AllArgsConstructor(suppressConstructorProperties = true)
 @Builder
-public class Contact implements Parcelable {
+public class Contact {
     private String lastName;
     private String firstName;
     private String phone;
     private String mail;
-
-    public Contact(Parcel in) {
-        lastName = in.readString();
-        firstName = in.readString();
-        phone = in.readString();
-        mail = in.readString();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(lastName);
-        dest.writeString(firstName);
-        dest.writeString(phone);
-        dest.writeString(mail);
-    }
-
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
-        public Contact createFromParcel(Parcel in) {
-            return new Contact(in);
-        }
-
-        public Contact[] newArray(int size) {
-            return new Contact[size];
-        }
-    };
 }
